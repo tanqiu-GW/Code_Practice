84baec6e9c6146b8fdc85fa4dd4d4dc50b80e950
pc_dell_inspiron_gw <957074637@qq.com>
2020-10-25 09:50:21 +0800

    修改队列接口代码

--- data_structrue/common/queue/queue.c
+++ data_structrue/common/queue/queue.c
@@ -1,73 +1,136 @@
 /*
- * @Description: queue.c
  * @Author: gw
- * @Date: 2019-05-29 20:39:31
+ * @Date: 2020-10-25 00:45:43
  * @LastEditors: gw
- * @LastEditTime: 2019-06-01 19:25:21
+ * @LastEditTime: 2020-10-25 09:45:28
+ * @Description: 循环队列接口实现
+ * @FilePath: /Code_Practice/data_structrue/common/queue/queue.c
  */
-/* 头文件 */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
+/* 头文件 */
 #include "queue.h"
 
 
-/* 静态队列 ----------start */
-
-/*  创建静态队列
-    输入:len_queue,队列的最大总长度;
-    len_elemen,每个队列元素的长度;
-    *buf,存放队列元素的buf区指针;
-    *queue,创建完成后的队列指针;
-    输出:是否创建成功
-*/
-Status queue_static_create(U32 len_queue, U32 len_element, void *buf, sQueue *queue)
+/* 静态循环队列接口-----start */
+/**
+ * @description:静态循环队列初始化
+ * @param:*queue,队列指针;
+ *        lenQueue,队列最大长度;
+ *        lenElement,队列中每个元素的长度
+ *        *queueDataBuf,存放队列元素的buf区指针
+ * @return:0-队列初始化成功, 1-队列初始化失败
+ */
+int initLoopQueue(ts_LoopQueue *queue, int lenQueue, int lenElement, void *queueDataBuf)
 {
-    U32 num_byte = 0;
+    int num_byte = 0;
 
-    queue->len_queue = len_queue;
-    queue->len_element = len_element;
-    queue->count = 0;
-    queue->put = queue->get = 0;    //初始化头指针和尾指针都为0,队列为空
-    num_byte = len_element*len_queue;   //队列元素数据空间共有num_byte个字节
-    
-    if(buf == NULL)
+    if((lenQueue == 0) || (lenElement == 0) || (queueDataBuf == NULL))
     {
-        printf("the specified buffer is error!");
-        return FAILED;
+        printf("queue init failed.\n");
+        return -1;
     }
-    if(num_byte > sizeof(buf))
+
+    queue->lenQueue = lenQueue;
+    queue->lenElement = lenElement;
+    queue->count = 0;
+    queue->front = 0;
+    queue->rear = 0;
+
+    num_byte = lenElement*lenQueue;
+    memset(queueDataBuf, 0, sizeof(char)*num_byte);
+    queue->elementBuf = queueDataBuf;
+
+    return 0;
+}
+
+/**
+ * @description:判断静态循环队列是否已满
+ * @param:*queue,队列指针;
+ * @return:0-未满, 1-已满
+ */
+int isFullLoopQueue(ts_LoopQueue *queue)
+{
+    if((queue->rear+1) % queue->lenQueue == queue->front)
     {
-        printf("queue length exceeds buffer length!\n");
-        return FAILED;
+        return 1;
     }
     else
     {
-        printf("create circular queue successful.\n");
+        return 0;
     }
-
-    memset(l_data_buf, 0, sizeof(char)*num_byte);
-    queue->data_buf = buf;
-    
-    return SUCCESS;
 }
-/* 静态队列 ----------end */
 
-
-
-
-/* 动态队列 ----------start */
-
-/*  创建动态队列
-    输入:len_queue,队列总长度;
-    len_elemen,每个队列元素的长度;
-    *queue,创建完成后的队列指针;
-    输出:是否创建成功
-*/
-Status queue_dynamic_create(U32 len_queue, U32 len_element, sQueue *queue)
+/**
+ * @description:判断静态循环队列是否为空
+ * @param:*queue,队列指针;
+ * @return:0-非空, 1-为空
+ */
+int isEmptyLoopQueue(ts_LoopQueue *queue)
 {
-    return SUCCESS;
+    if(queue->front == queue->rear)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
-/* 动态队列 ----------end */
 
+/**
+ * @description:静态循环队列,入队
+ * @param:*queue,队列指针;
+ *        *element,入队元素指针;
+ * @return:0-入队成功,-1-入队失败
+ */
+int enLoopQueue(ts_LoopQueue *queue, void *element)
+{
+    if(isFullLoopQueue(queue))
+    {
+        printf("queue is full, can't enQueue.\n");
+        return -1;
+    }
+    else
+    {
+        //queue->elementBuf[queue->rear] = *element;
+        memcpy(((char *)queue->elementBuf)+(queue->lenElement*queue->rear), element, queue->lenElement);
+        queue->rear = (queue->rear+1) % queue->lenQueue;
+        queue->count++;
+
+        return 0;
+    }
+}
+
+/**
+ * @description:静态循环队列,出队
+ * @param:*queue,队列指针;
+ *        *element,出队元素指针;
+ * @return:0-出队成功,-1-出队失败
+ */
+int deLoopQueue(ts_LoopQueue *queue, void *element)
+{
+    if(isEmptyLoopQueue(queue))
+    {
+        printf("queue is empty, can't deQueue.\n");
+        return -1;
+    }
+    else
+    {
+        //*element = queue->elementBuf[queue->front];
+        memcpy(element, ((char *)queue->elementBuf)+(queue->lenElement*queue->front), queue->lenElement);
+        queue->front = (queue->front+1) % queue->lenQueue;
+        queue->count--;
+
+        return 0;
+    }
+}
+
+/* 静态循环队列接口-----end */
+
+
+
+
+
+/* 动态循环队列接口-----start */
+
+/* 动态循环队列接口-----end */
--- data_structrue/common/queue/queue.h
+++ data_structrue/common/queue/queue.h
@@ -1,10 +1,12 @@
 /*
- * @Description: queue.h
  * @Author: gw
- * @Date: 2019-05-29 20:39:38
+ * @Date: 2020-10-25 00:45:43
  * @LastEditors: gw
- * @LastEditTime: 2019-07-10 22:13:43
+ * @LastEditTime: 2020-10-25 09:39:40
+ * @Description: 循环队列接口
+ * @FilePath: /Code_Practice/data_structrue/common/queue/queue.h
  */
+
 #ifndef __QUEUE_H__
 #define __QUEUE_H__
 
@@ -12,37 +14,43 @@
 extern "C"{
 #endif /* __cplusplus */
 
-
 /* 头文件 */
-#include "../common_type.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
 
-/* 类型定义 */
-typedef char ElemType;  //默认基本元素类型
 
-/* 队列结构(头进尾出) */
-typedef struct tagQueue
+/* 队列结构类型 */
+typedef struct tag_loop_queue
 {
-    U32 len_queue;  //长度队列可存储多少个元素
-    U32 len_element;//每个元素长度
-    U32 count;      //队列内当前元素个数
-    U32 get;        //尾指针
-    U32 put;        //头指针
-    void *data_buf; //数据存储指针
-}sQueue;
-
-/* 静态队列 ----------start */
-extern Status queue_static_create(U32 len_queue, U32 len_element, void *buf, sQueue *queue); //静态内存存储队列元素
-extern Status queue_static_put();
-extern Status queue_static_get();
-/* 静态队列 ----------end */
+    int count; //队列当前元素个数
+    int lenQueue; //队列可存储元素长度
+    int lenElement; //队列中每个元素的长度
+    int rear; //队尾下标,指向数组里的第一个元素的位置,每次添加一个元素时rear向后移动一个单位
+    int front; //队头下标,只想数组里的最后一个元素的下一个元素,每次删除一个元素时front向后移动一个单位
+    void *elementBuf; //队列元素数组指针
+}ts_LoopQueue;
 
 
+/* 静态循环队列接口-----start */
+extern int initLoopQueueStatic(ts_LoopQueue *queue, int lenQueue, int lenElement, void *queueDataBuf); //初始化静态循环队列
+extern int isFullLoopQueueStatic(ts_LoopQueue *queue); //0-未满, 1-已满
+extern int isEmptyLoopQueueStatic(ts_LoopQueue *queue); //0-非空, 1-为空
+extern int enLoopQueueStatic(ts_LoopQueue *queue, void *element); //入队列. 0-入队成功,-1-入队失败
+extern int deLoopQueueStatic(ts_LoopQueue *queue, void *element); //出队列. 0-出队成功,-1-出队失败
+/* 静态循环队列接口-----end */
 
-/* 静态队列 ----------start */
-extern Status queue_dynamic_create(U32 len_queue, U32 len_element, sQueue *queue);//动态内存存储队列元素
-extern Status queue_dynamic_put();
-extern Status queue_dynamic_get();
-/* 静态队列 ----------end */
+
+
+/* 动态循环队列接口-----start */
+extern int initLoopQueueDynamic(ts_LoopQueue *queue, int lenQueue, int lenElement, void *queueDataBuf); //初始化静态循环队列
+extern int isFullLoopQueueDynamic(ts_LoopQueue *queue); //0-未满, 1-已满
+extern int isEmptyLoopQueueDynamic(ts_LoopQueue *queue); //0-非空, 1-为空
+extern int enLoopQueueDynamic(ts_LoopQueue *queue, void *element); //入队列. 0-入队成功,-1-入队失败
+extern int deLoopQueueDynamic(ts_LoopQueue *queue, void *element); //出队列. 0-出队成功,-1-出队失败
+/* 动态循环队列接口-----end */
+
 
 
 #ifdef __cplusplus
